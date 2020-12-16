2878eef0dc2f39e735a70f46db5629c924338f41
Binh Nguyen <thanhbinh01234@gmail.com>
2020-12-17 00:56:58 +0700

    Fix

--- web/dashboard.py
+++ web/dashboard.py
@@ -9,15 +9,13 @@ from bson.json_util import dumps
 
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb://localhost:27017")
 DBS_NAME = 'twitter'
-COLLECTION_NAME0 = 'keywords'
-COLLECTION_NAME1 = 'hashtags'
+
 COLLECTION_NAME2 = 'counts'
 COLLECTION_NAME3 = 'ratio'
 COLLECTION_NAME4 = 'tracking_word'
 COLLECTION_NAME5 = 'users'
 COLLECTION_NAME6 = 'time'
-FIELDS0 = {'Keyword': True, 'Count': True, '_id': False}
-FIELDS1 = {'Hashtag': True, 'Count': True, '_id': False}
+
 FIELDS2 = {'_id': False}
 FIELDS3 = {'_id': False}
 FIELDS4 = {'_id': False}
@@ -28,98 +26,57 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template("dashboard.html")
-
-
-@app.route("/data/keywords")
-def keywords():
-	connection = MongoClient(MONGO_URL)
-	collection = connection[DBS_NAME][COLLECTION_NAME0]
-	projects = collection.find(projection=FIELDS0)
-	json_projects = []
-	for project in projects:
-		json_projects.append(project)
-	json_projects = json.dumps(json_projects, default=json_util.default)
-	connection.close()
-	return json_projects
-
-@app.route("/data/hashtags")
-def hashtags():
-        connection = MongoClient(MONGO_URL)
-        collection = connection[DBS_NAME][COLLECTION_NAME1]
-        projects = collection.find(projection=FIELDS1)
-        json_projects = []
-        for project in projects:
-                json_projects.append(project)
-        json_projects = json.dumps(json_projects, default=json_util.default)
-        connection.close()
-        return json_projects
+    return render_template("dashboard.html")
 
 @app.route("/data/counts")
 def counts():
-        connection = MongoClient(MONGO_URL)
-        collection = connection[DBS_NAME][COLLECTION_NAME2]
-        projects = collection.find(projection=FIELDS2)
-        json_projects = []
-        for project in projects:
-                json_projects.append(project)
-        json_projects = json.dumps(json_projects, default=json_util.default)
-        connection.close()
-        return json_projects
+    connection = MongoClient(MONGO_URL)
+    collection = connection[DBS_NAME][COLLECTION_NAME2]
+    projects = collection.find(projection=FIELDS2)
+    json_projects = []
+    for project in projects:
+        json_projects.append(project)
+    json_projects = json.dumps(json_projects, default=json_util.default)
+    connection.close()
+    return json_projects
 
 @app.route("/data/ratio")
 def ratio():
-        connection = MongoClient(MONGO_URL)
-        collection = connection[DBS_NAME][COLLECTION_NAME3]
-        projects = collection.find(projection=FIELDS3)
-        json_projects = []
-        for project in projects:
-                json_projects.append(project)
-        json_projects = json.dumps(json_projects, default=json_util.default)
-        connection.close()
-        return json_projects
+    connection = MongoClient(MONGO_URL)
+    sentiment_coll = connection[DBS_NAME][COLLECTION_NAME3]
+    count_coll = connection[DBS_NAME][COLLECTION_NAME2]
+    pos_all = list(sentiment_coll.find(projection=FIELDS3))
+    pos = (p['pos'] for p in pos_all)
+    total = sum(list(count_coll.find())[:len(pos_all)])
+    json_projects = json.dumps([
+        {
+            'PN': 'P',
+            'Ratio': 1 if total == 0 else pos / total
+        },
+        {
+            'PN': 'N',
+            'Ratio': 0 if total == 0 else (total - pos) / total
+        }
+    ])
+    # json_projects = []
+    # for project in projects:
+    #     json_projects.append(project)
+    # json_projects = json.dumps(json_projects, default=json_util.default)
+    connection.close()
+    return json_projects
 
 
 @app.route("/data/tracking_word")
 def tracking_word():
-        connection = MongoClient(MONGO_URL)
-        collection = connection[DBS_NAME][COLLECTION_NAME4]
-        projects = collection.find(projection=FIELDS4)
-        json_projects = []
-        for project in projects:
-                json_projects.append(project)
-        json_projects = json.dumps(json_projects, default=json_util.default)
-        connection.close()
-        return json_projects
-
-@app.route("/data/users")
-def users():
-        connection = MongoClient(MONGO_URL)
-        collection = connection[DBS_NAME][COLLECTION_NAME5]
-        projects = collection.find(projection=FIELDS5)
-        json_projects = []
-        for project in projects:
-                json_projects.append(project)
-        json_projects = json.dumps(json_projects, default=json_util.default)
-        connection.close()
-        return json_projects
-
-@app.route("/data/time")
-def time():
-        connection = MongoClient(MONGO_URL)
-        collection = connection[DBS_NAME][COLLECTION_NAME6]
-        projects = collection.find(projection=FIELDS6)
-        json_projects = []
-        for project in projects:
-                json_projects.append(project)
-        json_projects = json.dumps(json_projects, default=json_util.default)
-        connection.close()
-        return json_projects
-
+    connection = MongoClient(MONGO_URL)
+    collection = connection[DBS_NAME][COLLECTION_NAME4]
+    projects = collection.find(projection=FIELDS4)
+    json_projects = []
+    for project in projects:
+        json_projects.append(project)
+    json_projects = json.dumps(json_projects, default=json_util.default)
+    connection.close()
+    return json_projects
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0',port=5000,debug=True)
-
-
-
-
+    app.run(host='0.0.0.0',port=5000,debug=True)
--- web/static/js/track.js
+++ web/static/js/track.js
@@ -1,40 +1,19 @@
 //////////////////////////////////////////////////////////////////
 //
-// 
 //
-var track = d3.select("#track").append("svg")
-              .attr("height", 120)
-              .append("g")
-              .attr("transform",
-                    "translate(" + 10 + "," + 70 + ")");
+//
+var track = d3
+    .select('#track')
+    .append('svg')
+    .attr('height', 120)
+    .append('g')
+    .attr('transform', 'translate(' + 10 + ',' + 70 + ')')
 var tr = ''
 // load the data
-d3.json("data/tracking_word", function(error, data) {
-    data.forEach(function(d) {
-        tr = d.Tracking_word;
-    });
+d3.json('data/tracking_word', function (error, data) {
+    data.forEach(function (d) {
+        tr = d.Tracking_word
+    })
 
-
-  track.append("text") 
-       .text(tr)
-       .style("font-size","30px");
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    track.append('text').text(tr).style('font-size', '30px')
+})
